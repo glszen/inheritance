ce42e3129d6255a9ec0570819a7f9ac1993428ac
Gülşen <gulsenk48@hotmail.com>
2024-10-22 21:50:10 +0300

    Add project files.

---
+++ BaseKisi.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Channels;
+using System.Threading.Tasks;
+
+namespace inheritance
+{
+    public class BaseKisi
+    {
+        public string Ad;
+        public string Soyad;
+
+       void AdSoyad()
+        {
+            Console.WriteLine(Ad + Soyad);
+        }
+    }
+
+    public class ogrenci : BaseKisi
+    {
+        public string ogrenciNo;
+        
+        void ogrenciBilgi ()
+        {
+            Console.WriteLine(Ad + Soyad + ogrenciNo);
+        }
+    }
+
+    public class ogretmen : BaseKisi
+    {
+        public string Maas;
+
+        void ogretmenBilgi()
+        {
+            Console.WriteLine(Ad + Soyad + Maas);
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,8 @@
+﻿using inheritance;
+using System.ComponentModel;
+
+ogrenci ogrenci = new ogrenci() { Ad = "Emily ", Soyad = "Cooper", ogrenciNo = " 2154851420" };
+Console.WriteLine(ogrenci.Ad + ogrenci.Soyad + ogrenci.ogrenciNo);
+
+ogretmen ogretmen = new ogretmen() { Ad = "Mindy ", Soyad = "Chen", Maas = " 60.000 TL" };
+Console.WriteLine(ogretmen.Ad + ogretmen.Soyad + ogretmen.Maas);
